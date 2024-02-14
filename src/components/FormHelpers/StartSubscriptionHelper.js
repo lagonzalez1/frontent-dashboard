aab8a6f7967b590fa051ce86c6f2a369731b0706
Luis <otfgonzalez@gmail.com>
2024-02-13 22:35:35 -0800

    mainly working on completing and showing errors based on failed payments

--- src/components/FormHelpers/StartSubscriptionHelper.js
+++ src/components/FormHelpers/StartSubscriptionHelper.js
@@ -1,13 +1,14 @@
 import axios from "axios";
-import { getHeaders } from "../../auth/Auth"
+import { getHeaders, getStateData } from "../../auth/Auth"
 
 
 
 // Will need to send bid and uid to confirm subscription on MONGO ?
-export const startSubscriptionTest = (priceId) => {
+export const startSubscriptionTest = (priceId, bid) => {
     const header = getHeaders();
+    const { _, business} = getStateData();
     return new Promise((resolve, reject) => {
-        axios.post('/api/internal/create-checkout-session', {priceId}, header)
+        axios.post('/api/internal/create-checkout-session', {priceId, bid: business._id}, header)
         .then(response => {
             resolve(response)
         })
